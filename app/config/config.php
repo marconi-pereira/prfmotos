a77f90ec3b2afdad2ce94198e62617b4ae47abc1
MARCONIMP1 <marconipereirajp@hotmail.com>
2021-05-21 19:26:10 -0300

    Atualização de conecxão com banco de dados

--- app/config/config.php
+++ app/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/Logic_system';
+$config['base_url'] = 'https://prfmotos.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
